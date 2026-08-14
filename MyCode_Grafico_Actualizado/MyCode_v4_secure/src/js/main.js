ee880ce5266fab8ad9e199f236797c4145aa1fab
velez1010 <juan.velezdo@amigo.edu.co>
2026-08-14 07:22:54 -0500

    Delete MyCode_Grafico_Actualizado directory
    se elimino todo debido a errores que presenta

--- MyCode_Grafico_Actualizado/MyCode_v4_secure/src/js/main.js
+++
@@ -1,7 +0,0 @@
-import { initApp, renderPage } from './pages.js';
-import { registerCurrentPageRenderer } from './ui.js';
-
-document.addEventListener('DOMContentLoaded', async () => {
-  registerCurrentPageRenderer(renderPage);
-  await initApp();
-});
